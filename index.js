5fb3ffaf0e571d9aa852bb35e6b749d037313c85
leigh966 <leigh966@hotmail.com>
2023-12-01 16:29:40 +0000

    generating and returning a sessionId if the password is correct

--- index.js
+++ index.js
@@ -5,13 +5,33 @@ const app = express()
 
 app.use(express.json());
 
+// Including crypto module
+const crypto = require('crypto');
+
+var sessionId = null;
+
+
+function generateSessionId(res)
+{
+	// Calling randomBytes method with callback
+crypto.randomBytes(127, (err, buf) => {
+  if (err) {
+    // Prints error
+    console.log(err);
+    return;
+  }
+ 
+  sessionId = buf.toString('hex');
+  res.send(sessionId)
+});
+}
 
 app.post('/login', (req, res) => {
 	const json = req.body;
 	if(json.password == "password")
 	{
 		res.status(201)
-		res.send("Logged In")
+		generateSessionId(res);
 		return;
 	}
 	res.status(401)
