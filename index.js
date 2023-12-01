5f96eaeafb5b93267a70525c26478e97ea8fba12
leigh966 <leigh966@hotmail.com>
2023-12-01 17:30:37 +0000

    Added cryptographic password checking from .env file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express")
 const app = express() 
 
 app.use(express.json());
+require('dotenv').config()
 
 // Including crypto module
 const crypto = require('crypto');
@@ -11,8 +12,15 @@ const crypto = require('crypto');
 var sessionId = null;
 
 
+function comparePassword(password, storedSalt, storedHash) 
+{
+	const hash = crypto.pbkdf2Sync(password, storedSalt, 1000, 64, `sha512`).toString(`hex`);
+    return storedHash === hash;
+  }
+
 function generateSessionId(res)
 {
+	
 	// Calling randomBytes method with callback
 crypto.randomBytes(127, (err, buf) => {
   if (err) {
@@ -28,7 +36,7 @@ crypto.randomBytes(127, (err, buf) => {
 
 app.post('/login', (req, res) => {
 	const json = req.body;
-	if(json.password == "password")
+	if(comparePassword(json.password, process.env.SALT, process.env.PASSWORD_HASH))
 	{
 		res.status(201)
 		generateSessionId(res);
