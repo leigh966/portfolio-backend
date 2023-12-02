66137f5e08d9722a7b50d87f89fff30afe50d33c
leigh966 <leigh966@hotmail.com>
2023-12-02 11:08:20 +0000

    moved security functionality to the verify module

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const app = express()
 app.use(express.json());
 require('dotenv').config()
 
-// Including crypto module
-const crypto = require('crypto');
+
 
 
 // Connect to postgres db
@@ -25,37 +24,17 @@ pgClient.connect();
 var sessionId = null;
 
 
-function comparePassword(password, storedSalt, storedHash) 
-{
-	const hash = crypto.pbkdf2Sync(password, storedSalt, 1000, 64, `sha512`).toString(`hex`);
-    return storedHash === hash;
-  }
 
-function generateSessionId(res)
-{
-	
-	// Calling randomBytes method with callback
-crypto.randomBytes(127, (err, buf) => {
-  if (err) {
-    // Prints error
-    console.log(err);
-    return;
-  }
- 
-  sessionId = buf.toString('hex');
-  res.send(sessionId)
-});
-}
-const verifyOTP = require("./verify");
+const verify = require("./verify");
 const { Console } = require("console");
 app.post('/login', (req, res) => {
 	const json = req.body;
-	const passwordCorrect = comparePassword(json.password, process.env.SALT, process.env.PASSWORD_HASH);
-	const otpCorrect = verifyOTP(json.otp);
+	const passwordCorrect = verify.password(json.password, process.env.SALT, process.env.PASSWORD_HASH);
+	const otpCorrect = verify.otp(json.otp);
 	if(passwordCorrect && otpCorrect)
 	{
 		res.status(201)
-		generateSessionId(res);
+		verify.createSession(res);
 		return;
 	}
 	res.status(401)
--- verify.js
+++ verify.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 const key = process.env.SECRET_KEY
 
+// Including crypto module
+const crypto = require('crypto');
+
 function verifyOTP(otp) {
   const verified = speakeasy.totp.verify({
     secret: key,
@@ -14,4 +17,28 @@ function verifyOTP(otp) {
   return verified;
 }
 
-module.exports = verifyOTP;
+function verifyPassword(password, storedSalt, storedHash) 
+{
+	const hash = crypto.pbkdf2Sync(password, storedSalt, 1000, 64, `sha512`).toString(`hex`);
+    return storedHash === hash;
+  }
+
+function generateSessionId(res)
+{
+	
+	// Calling randomBytes method with callback
+crypto.randomBytes(127, (err, buf) => {
+  if (err) {
+    // Prints error
+    console.log(err);
+    return;
+  }
+ 
+  sessionId = buf.toString('hex');
+  res.send(sessionId)
+});
+}
+
+module.exports.otp = verifyOTP;
+module.exports.password = verifyPassword;
+module.exports.createSession = generateSessionId;
