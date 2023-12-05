94eea1bab646b9143dace03fc98d8ac1fa642088
leigh966 <leigh966@hotmail.com>
2023-12-05 23:29:22 +0000

    now allowing self-signed certificates

--- postgres_client.js
+++ postgres_client.js
@@ -4,7 +4,7 @@ function getClient()
 {
 // Connect to postgres db
 const pg = require("pg");
-const pgClient = new pg.Client(process.env.DATABASE_URL);
+const pgClient = new pg.Client(process.env.DATABASE_URL+"sslmode=require");
 pgClient.connect();
 //require("./setup_table").setup(pgClient);
 return pgClient;
