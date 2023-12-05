105e448780497270418fd79c50ecc01f0bfb6cab
leigh966 <leigh966@hotmail.com>
2023-12-05 22:55:57 +0000

    Changed postgres client to use database url rather than host, user, password, etc... seperately

--- postgres_client.js
+++ postgres_client.js
@@ -4,13 +4,7 @@ function getClient()
 {
 // Connect to postgres db
 const pg = require("pg");
-const pgClient = new pg.Client({
-	user: process.env.PGUSER,
-	host: process.env.PGHOST,
-	database: process.env.PGDATABASE,
-	password: process.env.PGPASSWORD,
-	port: process.env.PGPORT,
-});
+const pgClient = new pg.Client(process.env.PGDATABASE);
 pgClient.connect();
 //require("./setup_table").setup(pgClient);
 return pgClient;
