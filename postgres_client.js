6611d1a55337ef656aed9b9b0ca933ed08053b3f
leigh966 <leigh966@hotmail.com>
2023-12-05 23:06:06 +0000

    fixed name of database_url env variable

--- postgres_client.js
+++ postgres_client.js
@@ -4,7 +4,7 @@ function getClient()
 {
 // Connect to postgres db
 const pg = require("pg");
-const pgClient = new pg.Client(process.env.PGDATABASE);
+const pgClient = new pg.Client(process.env.DATABASE_URL);
 pgClient.connect();
 //require("./setup_table").setup(pgClient);
 return pgClient;
