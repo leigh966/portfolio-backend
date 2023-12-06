c01b20e1a4efa92ec9f5dd2845ea8e2f8cebb91c
leigh966 <leigh966@hotmail.com>
2023-12-06 14:56:06 +0000

    enabled cross-origin

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.use(express.json());
 require('dotenv').config()
 const sanitizer = require('sanitize');
 app.use(sanitizer.middleware);
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 const lowerCase = "qwertyuiopasdfghjklzxcvbnmm";
 const upperCase = lowerCase.toUpperCase();
