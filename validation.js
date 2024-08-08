936e549773c13af0072559efb27afb4514f8ab60
Leigh966 <leigh966@hotmail.com>
2024-08-08 18:02:16 +0100

    updated dangerous character processing to be more robust

--- validation.js
+++ validation.js
@@ -1,5 +1,14 @@
+function shouldSkipDangerousCharacterProcessing(s) {
+  return (
+    s == null ||
+    s == undefined ||
+    !(typeof s === "string" || s instanceof String)
+  );
+}
+
 export function restoreDangerousCharacters(s) {
-  const list = s.split("#");
+  if (shouldSkipDangerousCharacterProcessing(s)) return s;
+  let list = s.split("#");
   let output = "";
   for (let i = 0; i < list.length; i++) {
     if (i % 2 == 1) {
@@ -15,6 +24,8 @@ const lowerCase = "qwertyuiopasdfghjklzxcvbnmm";
 const upperCase = lowerCase.toUpperCase();
 const allowedCharacters = lowerCase + upperCase + "1234567890.";
 export function removeDangerousCharacters(s) {
+  if (shouldSkipDangerousCharacterProcessing(s)) return s;
+  let list = s.split("#");
   let output = "";
   for (let i = 0; i < s.length; i++) {
     if (!allowedCharacters.includes(s[i])) {
