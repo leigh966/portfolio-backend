138fe095ee14a22752a68dc7c70ee2d54dac8252
Leigh966 <leigh966@hotmail.com>
2024-08-08 18:05:58 +0100

    updated the getAllProjects endpoint to use the DatabaseEntity class

--- project-fetching.js
+++ project-fetching.js
@@ -1,25 +1,10 @@
 import { getClient } from "./postgres_client.js";
-import { restoreDangerousCharacters } from "./validation.js";
+import { DatabaseEntity } from "./DatabaseEntity.js";
 
 export function getAllProjects(req, res) {
   const pgClient = getClient();
-  pgClient.query("SELECT * FROM projects").then((dbRes) => {
-    let output = [];
-    for (let i = 0; i < dbRes.rows.length; i++) {
-      let outputRow = {};
-      outputRow.id = dbRes.rows[i].id;
-      outputRow.name = restoreDangerousCharacters(dbRes.rows[i].name);
-      outputRow.description = restoreDangerousCharacters(
-        dbRes.rows[i].description
-      );
-      outputRow.last_updated = dbRes.rows[i].last_updated;
-      outputRow.tagline = restoreDangerousCharacters(dbRes.rows[i].tagline);
-      outputRow.image_filename = dbRes.rows[i].image_filename
-        ? restoreDangerousCharacters(dbRes.rows[i].image_filename)
-        : null;
-
-      output.push(outputRow);
-    }
+  let projectEntity = new DatabaseEntity("projects", getClient());
+  projectEntity.getAll().then((output) => {
     res.status(200);
     res.send(JSON.stringify(output));
     return;
